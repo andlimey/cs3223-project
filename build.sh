00c75b3ee8562f07808bbeb986afad163d20f51c
YangJiyu98 <yang.jiyu.jy@gmail.com>
2021-02-25 03:22:16 +0800

    remove all files

--- build.sh
+++
@@ -1,3 +0,0 @@
-javac -d classes -classpath lib -sourcepath src src/QueryMain.java
-javac -d classes -classpath lib -sourcepath src src/ConvertTxtToTbl.java 
-javac -d classes -classpath lib -sourcepath src src/RandomDB.java
